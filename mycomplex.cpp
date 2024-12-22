8872a2e963a53328fe10a1661fcf7ad897f1fc6c
Alexandr Sherstnev <AlexandrSherstnev@gmail.com>
2024-12-22 15:16:30 +0500

    Комментарии в стиле Doxygen и добавление gitignore

--- mycomplex.cpp
+++ mycomplex.cpp
@@ -1,36 +1,71 @@
+/**
+ * @file mycomplex.cpp
+ * @brief Реализация класса Complex для работы с комплексными числами.
+ */
+
 #include <iostream>
 #include <cmath>
 #include "mycomplex.h"
 using namespace std;
 
+/**
+ * @brief Конструктор с параметрами.
+ * @param aRe Действительная часть.
+ * @param aIm Мнимая часть.
+ */
 Complex::Complex(double aRe, double aIm) {
     Re = aRe;
     Im = aIm;
 }
 
+/**
+ * @brief Конструктор копирования.
+ * @param aRval Объект класса Complex, который нужно скопировать.
+ */
 Complex::Complex(const Complex& aRval) {
     Re = aRval.Re;
     Im = aRval.Im;
 }
 
+/**
+ * @brief Деструктор. Обнуляет значения полей.
+ */
 Complex::~Complex() {
     Re = 0.0;
     Im = 0.0;
 }
 
+/**
+ * @brief Устанавливает значения действительной и мнимой частей.
+ * @param aRe Действительная часть.
+ * @param aIm Мнимая часть.
+ */
 void Complex::Set(double aRe, double aIm) {
     Re = aRe;
     Im = aIm;
 }
 
+/**
+ * @brief Преобразование комплексного числа в его модуль.
+ * @return Модуль комплексного числа.
+ */
 Complex::operator double() {
     return abs();
 }
 
+/**
+ * @brief Вычисляет модуль комплексного числа.
+ * @return Модуль.
+ */
 double Complex::abs() {
     return sqrt(Re * Re + Im * Im);
 }
 
+/**
+ * @brief Перегрузка оператора сложения для двух комплексных чисел.
+ * @param aRval Второе слагаемое.
+ * @return Результат сложения.
+ */
 Complex Complex::operator+(const Complex& aRval) {
     Complex Result;
     Result.Re = Re + aRval.Re;
@@ -38,6 +73,11 @@ Complex Complex::operator+(const Complex& aRval) {
     return Result;
 }
 
+/**
+ * @brief Перегрузка оператора вычитания для двух комплексных чисел.
+ * @param aRval Вычитаемое.
+ * @return Результат вычитания.
+ */
 Complex Complex::operator-(const Complex& aRval) {
     Complex Result;
     Result.Re = Re - aRval.Re;
@@ -45,6 +85,11 @@ Complex Complex::operator-(const Complex& aRval) {
     return Result;
 }
 
+/**
+ * @brief Перегрузка оператора сложения для комплексного числа и вещественного числа.
+ * @param aRval Вещественное число.
+ * @return Результат сложения.
+ */
 Complex Complex::operator+(const double& aRval) {
     Complex result;
     result.Re = Re + aRval;
@@ -52,12 +97,22 @@ Complex Complex::operator+(const double& aRval) {
     return result;
 }
 
+/**
+ * @brief Перегрузка оператора вычитания для комплексного числа и вещественного числа.
+ * @param aRval Вещественное число.
+ * @return Результат вычитания.
+ */
 Complex Complex::operator-(const double& aRval) {
     Complex Result(*this);
     Result.Re = Re - aRval;
     return Result;
 }
 
+/**
+ * @brief Перегрузка оператора умножения для двух комплексных чисел.
+ * @param aRval Второй множитель.
+ * @return Результат умножения.
+ */
 Complex Complex::operator*(const Complex& aRval) {
     Complex Result;
     Result.Re = Re * aRval.Re - Im * aRval.Im;
@@ -65,6 +120,11 @@ Complex Complex::operator*(const Complex& aRval) {
     return Result;
 }
 
+/**
+ * @brief Перегрузка оператора умножения для комплексного числа и вещественного числа.
+ * @param aRval Вещественное число.
+ * @return Результат умножения.
+ */
 Complex Complex::operator*(const double& aRval) {
     Complex Result;
     Result.Re = Re * aRval;
@@ -72,6 +132,11 @@ Complex Complex::operator*(const double& aRval) {
     return Result;
 }
 
+/**
+ * @brief Перегрузка оператора деления для комплексного числа и вещественного числа.
+ * @param aRval Вещественное число.
+ * @return Результат деления.
+ */
 Complex Complex::operator/(const double& aRval) {
     Complex Result;
     Result.Re = Re / aRval;
@@ -79,18 +144,33 @@ Complex Complex::operator/(const double& aRval) {
     return Result;
 }
 
+/**
+ * @brief Перегрузка оператора += для двух комплексных чисел.
+ * @param aRval Слагаемое.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator+=(const Complex& aRval) {
     Re += aRval.Re;
     Im += aRval.Im;
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора -= для двух комплексных чисел.
+ * @param aRval Вычитаемое.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator-=(const Complex& aRval) {
     Re -= aRval.Re;
     Im -= aRval.Im;
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора *= для двух комплексных чисел.
+ * @param aRval Множитель.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator*=(const Complex& aRval) {
     double tmpRe = Re;
     Re = Re * aRval.Re - Im * aRval.Im;
@@ -98,46 +178,88 @@ Complex& Complex::operator*=(const Complex& aRval) {
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора += для комплексного числа и вещественного числа.
+ * @param aRval Слагаемое.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator+=(const double& aRval) {
     Re += aRval;
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора -= для комплексного числа и вещественного числа.
+ * @param aRval Вычитаемое.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator-=(const double& aRval) {
     Re -= aRval;
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора *= для комплексного числа и вещественного числа.
+ * @param aRval Множитель.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator*=(const double& aRval) {
     Re *= aRval;
     Im *= aRval;
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора /= для комплексного числа и вещественного числа.
+ * @param aRval Делитель.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator/=(const double& aRval) {
     Re /= aRval;
     Im /= aRval;
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора = для двух комплексных чисел.
+ * @param aRval Присваиваемое значение.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator=(const Complex& aRval) {
     Re = aRval.Re;
     Im = aRval.Im;
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора = для комплексного числа и вещественного числа.
+ * @param aRval Присваиваемое значение.
+ * @return Ссылка на текущий объект.
+ */
 Complex& Complex::operator=(const double& aRval) {
     Re = aRval;
     Im = 0.0;
     return *this;
 }
 
+/**
+ * @brief Перегрузка оператора ввода из потока.
+ * @param stream Входной поток.
+ * @param a Объект класса Complex для ввода значений.
+ * @return Ссылка на поток.
+ */
 istream& operator>>(istream& stream, Complex& a) {
     char tmp[256];
     stream >> a.Re >> a.Im >> tmp;
     return stream;
 }
 
+/**
+ * @brief Перегрузка оператора вывода в поток.
+ * @param stream Выходной поток.
+ * @param a Объект класса Complex для вывода.
+ * @return Ссылка на поток.
+ */
 ostream& operator<<(ostream& stream, Complex& a) {
     stream << a.Re;
     if (!(a.Im < 0)) stream << '+';
@@ -145,6 +267,12 @@ ostream& operator<<(ostream& stream, Complex& a) {
     return stream;
 }
 
+/**
+ * @brief Перегрузка оператора сложения для вещественного числа и комплексного числа.
+ * @param aLval Вещественное число.
+ * @param aRval Комплексное число.
+ * @return Результат сложения.
+ */
 Complex operator+(const double& aLval, const Complex& aRval) {
     Complex Result;
     Result.Re = aLval + aRval.Re;
@@ -152,6 +280,12 @@ Complex operator+(const double& aLval, const Complex& aRval) {
     return Result;
 }
 
+/**
+ * @brief Перегрузка оператора вычитания для вещественного числа и комплексного числа.
+ * @param aLval Вещественное число.
+ * @param aRval Комплексное число.
+ * @return Результат вычитания.
+ */
 Complex operator-(const double& aLval, const Complex& aRval) {
     Complex Result;
     Result.Re = aLval - aRval.Re;
@@ -159,6 +293,12 @@ Complex operator-(const double& aLval, const Complex& aRval) {
     return Result;
 }
 
+/**
+ * @brief Перегрузка оператора умножения для вещественного числа и комплексного числа.
+ * @param aLval Вещественное число.
+ * @param aRval Комплексное число.
+ * @return Результат умножения.
+ */
 Complex operator*(const double& aLval, const Complex& aRval) {
     Complex r;
     r.Re = aLval * aRval.Re;
--- mycomplex.h
+++ mycomplex.h
@@ -2,38 +2,205 @@
 #define _MY_COMPLEX_H_
 #include <iostream>
 using namespace std;
+
+/**
+ * @class Complex
+ * @brief Класс для работы с комплексными числами.
+ */
 class Complex
-{double Re;
-	double Im; 
-	public:
-Complex(double aRe=0, double aIm=0);
-Complex(const Complex&);
-~Complex(    );
-void Set(double aRe, double aIm=0);
-operator double();
- double abs();
- friend istream& operator>>(   istream&,Complex&) ; 
+{
+    double Re; /**< Действительная часть комплексного числа. */
+    double Im; /**< Мнимая часть комплексного числа. */
 
+public:
+    /**
+     * @brief Конструктор с параметрами по умолчанию.
+     * @param aRe Действительная часть (по умолчанию 0).
+     * @param aIm Мнимая часть (по умолчанию 0).
+     */
+    Complex(double aRe = 0, double aIm = 0);
 
+    /**
+     * @brief Конструктор копирования.
+     * @param a Другой объект Complex для копирования.
+     */
+    Complex(const Complex& a);
 
-friend ostream& operator<< (ostream&, Complex & );
+    /**
+     * @brief Деструктор.
+     */
+    ~Complex();
 
-Complex operator+ (const Complex&);
-Complex operator- (const Complex&);
-Complex operator+ (const double&);
-friend 		Complex operator+ (const double&, const Complex&);
-Complex operator- (const double&);
-friend Complex operator- (const 	double&, const Complex&);
-Complex operator* (const Complex&);
-Complex operator* (const double&);
-friend Complex operator* (const double&, const Complex&)    ;Complex operator/ (const double&);
+    /**
+     * @brief Устанавливает значение комплексного числа.
+     * @param aRe Новое значение действительной части.
+     * @param aIm Новое значение мнимой части (по умолчанию 0).
+     */
+    void Set(double aRe, double aIm = 0);
 
-    Complex& operator+= (const Complex&);
-Complex  &  operator-= (const Complex&)    ;
-    Complex& operator*= (const Complex& );  Complex& operator  += (const double&);
-Complex& operator-= (const double&);
-Complex  &operator  *=(const double  & );
-Complex  & operator /=( const   double & );
-Complex& operator = (  const   Complex&);
-Complex & operator= (const double&);};
+    /**
+     * @brief Преобразует объект Complex в его модуль (тип double).
+     * @return Модуль комплексного числа.
+     */
+    operator double();
+
+    /**
+     * @brief Вычисляет модуль комплексного числа.
+     * @return Модуль числа (sqrt(Re^2 + Im^2)).
+     */
+    double abs();
+
+    /**
+     * @brief Перегрузка оператора ввода для класса Complex.
+     * @param stream Входной поток.
+     * @param a Объект Complex для ввода.
+     * @return Ссылка на входной поток.
+     */
+    friend istream& operator>>(istream& stream, Complex& a);
+
+    /**
+     * @brief Перегрузка оператора вывода для класса Complex.
+     * @param stream Выходной поток.
+     * @param a Объект Complex для вывода.
+     * @return Ссылка на выходной поток.
+     */
+    friend ostream& operator<<(ostream& stream, Complex& a);
+
+    /**
+     * @brief Перегрузка оператора сложения для двух объектов Complex.
+     * @param a Другой объект Complex.
+     * @return Результат сложения.
+     */
+    Complex operator+(const Complex& a);
+
+    /**
+     * @brief Перегрузка оператора вычитания для двух объектов Complex.
+     * @param a Другой объект Complex.
+     * @return Результат вычитания.
+     */
+    Complex operator-(const Complex& a);
+
+    /**
+     * @brief Перегрузка оператора сложения для Complex и double.
+     * @param a Число типа double.
+     * @return Результат сложения.
+     */
+    Complex operator+(const double& a);
+
+    /**
+     * @brief Перегрузка оператора сложения для double и Complex.
+     * @param aLval Число типа double.
+     * @param aRval Объект Complex.
+     * @return Результат сложения.
+     */
+    friend Complex operator+(const double& aLval, const Complex& aRval);
+
+    /**
+     * @brief Перегрузка оператора вычитания для Complex и double.
+     * @param a Число типа double.
+     * @return Результат вычитания.
+     */
+    Complex operator-(const double& a);
+
+    /**
+     * @brief Перегрузка оператора вычитания для double и Complex.
+     * @param aLval Число типа double.
+     * @param aRval Объект Complex.
+     * @return Результат вычитания.
+     */
+    friend Complex operator-(const double& aLval, const Complex& aRval);
+
+    /**
+     * @brief Перегрузка оператора умножения для двух объектов Complex.
+     * @param a Другой объект Complex.
+     * @return Результат умножения.
+     */
+    Complex operator*(const Complex& a);
+
+    /**
+     * @brief Перегрузка оператора умножения для Complex и double.
+     * @param a Число типа double.
+     * @return Результат умножения.
+     */
+    Complex operator*(const double& a);
+
+    /**
+     * @brief Перегрузка оператора умножения для double и Complex.
+     * @param aLval Число типа double.
+     * @param aRval Объект Complex.
+     * @return Результат умножения.
+     */
+    friend Complex operator*(const double& aLval, const Complex& aRval);
+
+    /**
+     * @brief Перегрузка оператора деления для Complex и double.
+     * @param a Число типа double.
+     * @return Результат деления.
+     */
+    Complex operator/(const double& a);
+
+    /**
+     * @brief Перегрузка оператора += для двух объектов Complex.
+     * @param a Другой объект Complex.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator+=(const Complex& a);
+
+    /**
+     * @brief Перегрузка оператора -= для двух объектов Complex.
+     * @param a Другой объект Complex.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator-=(const Complex& a);
+
+    /**
+     * @brief Перегрузка оператора *= для двух объектов Complex.
+     * @param a Другой объект Complex.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator*=(const Complex& a);
+
+    /**
+     * @brief Перегрузка оператора += для Complex и double.
+     * @param a Число типа double.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator+=(const double& a);
+
+    /**
+     * @brief Перегрузка оператора -= для Complex и double.
+     * @param a Число типа double.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator-=(const double& a);
+
+    /**
+     * @brief Перегрузка оператора *= для Complex и double.
+     * @param a Число типа double.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator*=(const double& a);
+
+    /**
+     * @brief Перегрузка оператора /= для Complex и double.
+     * @param a Число типа double.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator/=(const double& a);
+
+    /**
+     * @brief Перегрузка оператора присваивания для двух объектов Complex.
+     * @param a Другой объект Complex.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator=(const Complex& a);
+
+    /**
+     * @brief Перегрузка оператора присваивания для Complex и double.
+     * @param a Число типа double.
+     * @return Ссылка на текущий объект.
+     */
+    Complex& operator=(const double& a);
+};
+
 #endif
